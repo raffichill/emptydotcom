3288888582e3fa7965d53492f0d3d9b691276a3a
Cory Etzkorn <coryetzkorn@gmail.com>
2024-03-05 10:40:11 -0500

    Video loading

--- components/Demo/index.tsx
+++ components/Demo/index.tsx
@@ -1,11 +1,14 @@
 "use client"
 
+import classNames from "classnames"
 import Image from "next/image"
+import { useState } from "react"
 import styles from "./styles.module.css"
 
 export default function Demo() {
+  const [isLoaded, setIsLoaded] = useState(false)
   return (
-    <div className={styles.demo}>
+    <div className={classNames(styles.demo)}>
       <Image
         className={styles.demoDevice}
         src="/iphone.png"
